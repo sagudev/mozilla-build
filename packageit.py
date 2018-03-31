7d469cb201c645535c7c2c2c0b906477110d41c2
Ryan VanderMeulen <ryanvm@gmail.com>
2018-03-31 10:05:11 -0400

    Bug 1449917 - Copy watchman to the bin directory during packaging.

--- packageit.py
+++ packageit.py
@@ -246,8 +246,10 @@ copyfile(join(sourcedir, "vswhere.exe"), join(pkgdir, r"bin\vswhere.exe"))
 # Extract watchman to the stage directory.
 print "Staging watchman..."
 with zipfile.ZipFile(join(sourcedir, "watchman-d8501b7.zip"), 'r') as watchman_zip:
-    watchman_zip.extractall(join(pkgdir, "watchman"))
-os.remove(join(pkgdir, r"watchman\watchman.pdb"))
+    watchman_zip.extractall(join(pkgdir, r"bin\watchman-d8501b7"))
+os.remove(join(pkgdir, r"bin\watchman-d8501b7\watchman.pdb"))
+# Copy watchman.exe to the main bin directory to make our PATH bit more tidy
+copyfile(join(pkgdir, r"bin\watchman-d8501b7\watchman.exe"), join(pkgdir, r"bin\watchman.exe"))
 
 # Extract wget to the stage directory.
 # Downloaded from https://eternallybored.org/misc/wget/
