d7d65b9dec0e9d8400d4ca1acd39f16907116093
Marc Streckfuss <marc.streckfuss@gmail.com>
2019-12-21 23:38:49 +0100

    Bug 1603273 - Expose Cargo on $PATH, r=glob

--- msys/misc/profile-extravars.sh
+++ msys/misc/profile-extravars.sh
@@ -2,7 +2,8 @@
 
 if test -n "$MOZILLABUILD"; then
     MSYS_MOZBUILD=$(cd "$MOZILLABUILD" && pwd)
-    PATH="/local/bin:$MSYS_MOZBUILD/bin:$MSYS_MOZBUILD/kdiff3:$MSYS_MOZBUILD/nsis-3.01:$MSYS_MOZBUILD/python:$MSYS_MOZBUILD/python/Scripts:$MSYS_MOZBUILD/python3:$MSYS_MOZBUILD/python3/Scripts:$PATH"
+    MSYS_USERPROFILE=$(cd "$USERPROFILE" && pwd) # converts the path to linux style
+    PATH="/local/bin:$MSYS_USERPROFILE/.cargo/bin:$MSYS_MOZBUILD/bin:$MSYS_MOZBUILD/kdiff3:$MSYS_MOZBUILD/nsis-3.01:$MSYS_MOZBUILD/python:$MSYS_MOZBUILD/python/Scripts:$MSYS_MOZBUILD/python3:$MSYS_MOZBUILD/python3/Scripts:$PATH"
     EDITOR="emacs.exe"
     export PATH EDITOR
     export HOSTTYPE MACHTYPE OSTYPE SHELL
