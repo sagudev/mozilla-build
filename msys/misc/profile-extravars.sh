d8b8f7f755f763cef98d5bd4cf0fae28bc1b6592
Mitchell Hentges <mhentges@mozilla.com>
2021-11-10 16:06:51 +0000

    Bug 1740357: Maintain cargo's entry in the PATH r=glandium
    Rather than manually finding `cargo` after-the-fact, tweak the "reset
    path" behaviour to ensure that `cargo`'s dir is maintained.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130807

--- msys/misc/profile-extravars.sh
+++ msys/misc/profile-extravars.sh
@@ -2,8 +2,7 @@
 
 if test -n "$MOZILLABUILD"; then
     MSYS_MOZBUILD=$(cd "$MOZILLABUILD" && pwd)
-    MSYS_USERPROFILE=$(cd "$USERPROFILE" && pwd) # converts the path to linux style
-    PATH="/local/bin:$MSYS_USERPROFILE/.cargo/bin:$MSYS_MOZBUILD/bin:$MSYS_MOZBUILD/kdiff3:$MSYS_MOZBUILD/nsis-3.01:$MSYS_MOZBUILD/python:$MSYS_MOZBUILD/python/Scripts:$MSYS_MOZBUILD/python3:$MSYS_MOZBUILD/python3/Scripts:$PATH"
+    PATH="/local/bin:$MSYS_MOZBUILD/bin:$MSYS_MOZBUILD/kdiff3:$MSYS_MOZBUILD/nsis-3.01:$MSYS_MOZBUILD/python:$MSYS_MOZBUILD/python/Scripts:$MSYS_MOZBUILD/python3:$MSYS_MOZBUILD/python3/Scripts:$PATH"
     EDITOR="emacs.exe"
     export PATH EDITOR
     export HOSTTYPE MACHTYPE OSTYPE SHELL
