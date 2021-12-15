74fbce1825186d78f22ab5757cbde47d77ef7d1e
Mitchell Hentges <mhentges@mozilla.com>
2021-12-15 16:49:55 +0000

    Bug 1739443: Remove old nsis/mozmake files from existing installations r=glandium
    In pave-over installs (installations where old files were not removed)
    the old nsis and mozmake files will remain unless manually removed.
    Add manual deletion to the installer script.
    
    Also, remove obsolete NSIS entry from the PATH.
    
    Differential Revision: https://phabricator.services.mozilla.com/D133542

--- msys/misc/profile-extravars.sh
+++ msys/misc/profile-extravars.sh
@@ -2,7 +2,7 @@
 
 if test -n "$MOZILLABUILD"; then
     MSYS_MOZBUILD=$(cd "$MOZILLABUILD" && pwd)
-    PATH="/local/bin:$MSYS_MOZBUILD/bin:$MSYS_MOZBUILD/kdiff3:$MSYS_MOZBUILD/nsis-3.01:$MSYS_MOZBUILD/python3:$MSYS_MOZBUILD/python3/Scripts:$MSYS_MOZBUILD/python:$MSYS_MOZBUILD/python/Scripts:$PATH"
+    PATH="/local/bin:$MSYS_MOZBUILD/bin:$MSYS_MOZBUILD/kdiff3:$MSYS_MOZBUILD/python3:$MSYS_MOZBUILD/python3/Scripts:$MSYS_MOZBUILD/python:$MSYS_MOZBUILD/python/Scripts:$PATH"
     EDITOR="emacs.exe"
     export PATH EDITOR
     export HOSTTYPE MACHTYPE OSTYPE SHELL
