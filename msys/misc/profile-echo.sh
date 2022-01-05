55cab2c8149ab0e08e29c802740a8889da4ccae7
Mitchell Hentges <mhentges@mozilla.com>
2022-01-05 22:29:52 +0000

    Bug 1725895: Clean up "msys\misc\" directory r=glandium
    Pull the msys config files up from their nested directory and name their
    new folder accordingly.
    
    Combine the various "profile-*" files.
    
    Differential Revision: https://phabricator.services.mozilla.com/D134572

--- msys/misc/profile-echo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# $MOZILLABUILD should always be set by start-shell.bat.
-echo "MozillaBuild Install Directory: ${MOZILLABUILD}"
--- msys/misc/profile-extravars.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export EDITOR="emacs.exe"
-if test -n "$MOZILLABUILD"; then
-    PATH="$MOZILLABUILD/bin:$MOZILLABUILD/kdiff3:$MOZILLABUILD/python3:$MOZILLABUILD/python3/Scripts:$MOZILLABUILD/python:$MOZILLABUILD/python/Scripts:$PATH"
-fi
