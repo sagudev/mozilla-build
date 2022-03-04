d58c35745a469e47d84446a4f6c20ea13e007919
Mitchell Hentges <mhentges@mozilla.com>
2022-03-04 15:15:22 +0000

    Bug 1751500: Re-add support for the `USE_MINTTY` option r=glandium
    The `USE_MINTTY` option was originally removed as part of Bug 1725895,
    since Mintty was going to be used by default.
    
    However, an earlier patch (part of bug 1751500) had to revert the
    terminal change and make the Command Prompt the default terminal again.
    
    Since the terminal situation is back to how it was with MozillaBuild
    3.4, we might as well continue to export `USE_MINTTY` to avoid a
    breaking change. For what it's worth, Mintty is //more// viable today
    than it was with MozillaBuild 3.4 due to ConPTY and the updated MSYS2
    environment, so keeping it accessible is a good idea.
    
    Related, as part of this: move the conpty-enabling line to only happen
    if Mintty is used, because otherwise it's unnecessary.
    
    Differential Revision: https://phabricator.services.mozilla.com/D137799

--- start-shell.bat
+++ start-shell.bat
@@ -7,14 +7,26 @@ SET CYGWIN=
 SET INCLUDE=
 SET LIB=
 SET GITDIR=
-REM Opt into "ConPTY" support, which enables usage of win32 console binaries from MSYS2.
-SET MSYS=enable_pcon
 SET MOZILLABUILD=%~dp0
+
+REM mintty is available as an alternate terminal, but is not enabled by default due
+REM to spurious newlines being added to copied text - see bug 1751500.
+IF NOT DEFINED USE_MINTTY (
+  SET USE_MINTTY=
+)
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`where ssh 2^>NUL`) DO (
     SET EXTERNAL_TO_MOZILLABUILD_SSH_DIR=%%~dpF
 )
 
 REM Start shell.
-%MOZILLABUILD%msys2\msys2_shell.cmd -no-start -defterm -full-path %*
+IF "%USE_MINTTY%" == "1" (
+  REM Opt into "ConPTY" support, which enables usage of win32 console binaries when
+  REM running from mintty
+  SET MSYS=enable_pcon
+  %MOZILLABUILD%msys2\msys2_shell.cmd -full-path %*
+) ELSE (
+  %MOZILLABUILD%msys2\msys2_shell.cmd -no-start -defterm -full-path %*
+)
+
 EXIT /B
